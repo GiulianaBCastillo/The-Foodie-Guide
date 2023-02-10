6a92b8042d8f116063577d0b1ce1574f56b15dc0
GiulianaBCastillo <giuliana.b.castillo@gmail.com>
2023-02-10 08:53:03 +0000

    Edaman API tested and added to the code. Working good. Calories values added to the console

--- script.js
+++ script.js
@@ -14,6 +14,22 @@ $("#random-meal").on("click", function() {
       });
   });
 
+  $("#nutrition-value").on("click", function () {
+    var queryURL = "https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=pasta&app_id=36865b74&app_key=9cf0e140b88b31c2052ee297822d09de";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function(response) {
+        console.log(response);
+        console.log(response.hits);
+        console.log(response.hits[0].recipe)
+        console.log(response.hits[0].recipe.calories)
+
+
+    })
+  })
 
 
 
