5766c4ce2382167aab14938a0dd4f7dac8164391
Alhassan Dumbuya <alhassandumbuya@Alhassans-MacBook-Air.local>
2023-02-10 08:52:21 +0000

    adding the random meal intro fucntion

--- script.js
+++ script.js
@@ -8,18 +8,27 @@ $("#random-meal").on("click", function () {
     method: "GET"
   })
     .then(function (response) {
-      console.log(response); //response from url
-      console.log(response.meals[0]); // meal array
-      console.log(response.meals[0].strMeal); //name of the meal
-      console.log(response.meals[0].strInstructions); // recipe steps
-      console.log(response.meals[0].strYoutube); // video link for the meal
-      let videoId = response.meals[0].strYoutube.split("="); //using the split function to get url and the vid id number
-      console.log(videoId);
-      let vidSource = "https://www.youtube.com/embed/" + videoId[1]; // used the embeded video link url and added the videoID number to call the video
-      console.log(vidSource);
-      let iFrame = document.createElement("iframe"); // an iframe element is created to display the video on to the webpage 
-      iFrame.src = vidSource; // the iframe src will be the vidSource link 
-      document.body.appendChild(iFrame); // appending(adding) the video source on to the html document 
+        //response from url
+      console.log(response); 
+      // meal array
+      console.log(response.meals[0]); 
+      //name of the meal
+      console.log(response.meals[0].strMeal); 
+      let randomMeal = 'Your random meal is : ' + response.meals[0].strMeal
+      console.log(randomMeal);
+
+
+
+
+    //   console.log(response.meals[0].strInstructions); // recipe steps
+    //   console.log(response.meals[0].strYoutube); // video link for the meal
+    //   let videoId = response.meals[0].strYoutube.split("="); //using the split function to get url and the vid id number
+    //   console.log(videoId);
+    //   let vidSource = "https://www.youtube.com/embed/" + videoId[1]; // used the embeded video link url and added the videoID number to call the video
+    //   console.log(vidSource);
+    //   let iFrame = document.createElement("iframe"); // an iframe element is created to display the video on to the webpage 
+    //   iFrame.src = vidSource; // the iframe src will be the vidSource link 
+    //   document.body.appendChild(iFrame); // appending(adding) the video source on to the html document 
     
   });
 });
