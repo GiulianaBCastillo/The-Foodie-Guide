f4eb62aa4461876f95744b4b4c0eec8cbef1ae75
Alhassan Dumbuya <alhassandumbuya@Alhassans-MacBook-Air.local>
2023-02-10 08:41:25 +0000

    completed the comments

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ $("#random-meal").on("click", function () {
       console.log(response.meals[0].strMeal); //name of the meal
       console.log(response.meals[0].strInstructions); // recipe steps
       console.log(response.meals[0].strYoutube); // video link for the meal
-      let videoId = response.meals[0].strYoutube.split("=");
+      let videoId = response.meals[0].strYoutube.split("="); //using the split function to get url and the vid id number
       console.log(videoId);
-      let vidSource = "https://www.youtube.com/embed/" + videoId[1];
+      let vidSource = "https://www.youtube.com/embed/" + videoId[1]; // used the embeded video link url and added the videoID number to call the video
       console.log(vidSource);
-      let iFrame = document.createElement("iframe");
-      iFrame.src = vidSource;
-      document.body.appendChild(iFrame);
+      let iFrame = document.createElement("iframe"); // an iframe element is created to display the video on to the webpage 
+      iFrame.src = vidSource; // the iframe src will be the vidSource link 
+      document.body.appendChild(iFrame); // appending(adding) the video source on to the html document 
     
   });
 });
