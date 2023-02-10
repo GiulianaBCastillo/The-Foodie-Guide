7e2ed3e5771556208d68d0ac6b1ba3ff708b3be4
Alhassan Dumbuya <alhassandumbuya@Alhassans-MacBook-Air.local>
2023-02-10 08:23:52 +0000

    added data collection points from the APi

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-$("#random-meal").on("click", function() {
-  
-    //
-    var queryURL = "https://www.themealdb.com/api/json/v1/1/random.php";
+$("#random-meal").on("click", function () {
+  //
+  var queryURL = "https://www.themealdb.com/api/json/v1/1/random.php";
 
-    //
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-
-
-
-
-
-
-
-
-
-
-    });
- });
+  //
+  $.ajax({
+    url: queryURL,
+    method: "GET".then(function (response) {
+      console.log(response); //response from url
+      console.log(response.meals[0]); // meal array
+      console.log(response.meals[0].strMeal); //name of the meal
+      console.log(response.meals[0].strInstructions); // recipe steps
+      console.log(response.meals[0].strYoutube); // video link for the meal
+      let videoId = response.meals[0].strYoutube.split("=");
+      console.log(videoId);
+      let vidSource = "https://www.youtube.com/embed/" + videoId[1];
+      console.log(vidSource);
+      let iFrame = document.createElement("iframe");
+      iFrame.src = vidSource;
+      document.body.appendChild(iFrame);
+    }),
+  });
+});
