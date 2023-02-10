803754d7c26ac54336925b0478b97d599ba74279
GiulianaBCastillo <giuliana.b.castillo@gmail.com>
2023-02-10 07:45:29 +0000

    html and js files changed completely. API key now is logging the name of the recipe in the console.

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-////testing API // is not working at the moment,see can you reckon from the below link
-// https://developer.edamam.com/edamam-docs-nutrition-api
-var ingredient  = $("#search-input");
-var searchButton = $("#search-button");
-var searchFood=[];
-$("#search-button").on("click", DisplayNutrition);
-
-function nutrition(food){
-    var queryURL= "https://api.edamam.com/api/recipes/v2?type=public&q=pasta&app_id=299b322d&app_key=c2e37f835315905a8c42461585f9c738&ingr=5" + ingredient;
-    $.ajax({
-        url:queryURL,
-        method:"GET",
-    }).then(function(response){
-  
-      console.log(response.food); })}
-function DisplayNutrition(event){
-    event.preventDefault();
-    if(ingredient.val().trim()!==""){
-        food=ingredient.val().trim();
-        nutrition(food);
-    }
+$("#random-meal").on("click", function() {
     
-}
+    var queryURL = "https://api.edamam.com/api/recipes/v2?type=public&q=pasta&app_id=299b322d&app_key=c2e37f835315905a8c42461585f9c738&ingr=5";
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+        console.log(response);
+        console.log(response.hits);
+        console.log(response.hits[0].recipe.label);
+        //console.log(response);
+      });
+  });
+
+
+
+
+
+
+
+
+
+
