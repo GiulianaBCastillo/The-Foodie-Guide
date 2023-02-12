12ecc9107e84bdd09cab4dde2885ac4b9f6b1212
Alhassan Dumbuya <alhassandumbuya@Alhassans-MacBook-Air.local>
2023-02-12 20:21:32 +0000

    changes to the mesurement

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-$("#random-meal").on("click", function () {
+let randomMealImage = $("#random-meal-img");
+// let randomMealVideo = $("#random-meal-video");
+
+$("#random-meal").on("click", function (e) {
+  e.preventDefault();
+  // $(".ramdom-meal-video").empty();
   //
   var queryURL = "https://www.themealdb.com/api/json/v1/1/random.php";
 
@@ -8,15 +13,15 @@ $("#random-meal").on("click", function () {
     method: "GET",
   }).then(function (response) {
     //response from url
-    console.log(response);
+    // console.log(response);
     // meal array
     let meals = response.meals;
-    console.log(meals);
-    console.log(meals[0].strIngredient1);
+    // console.log(meals);
+    // console.log(meals[0].strIngredient1);
 
     //name of the meal
 
-    console.log(response.meals[0].strMeal);
+    // console.log(response.meals[0].strMeal);
     let randomMeal =
       "Your random meal is : " +
       response.meals[0].strMeal +
@@ -24,12 +29,70 @@ $("#random-meal").on("click", function () {
       "👏" +
       "👏" +
       "😍";
-    console.log(randomMeal);
+    // console.log(randomMeal);
     var randomMealtitle = $("<h1></h1>");
     randomMealtitle.text(randomMeal);
-    randomMealtitle.appendTo("#food-title");
+    randomMealtitle.appendTo("#meal-title");
 
     //ingredient list
+    let randomMealIngredients = response.meals[0];
+    // console.log(randomMealIngredients);
+
+    let randomMealMeasurementArr = [
+      randomMealIngredients.strMeasure1,
+      randomMealIngredients.strMeasure2,
+      randomMealIngredients.strMeasure3,
+      randomMealIngredients.strMeasure4,
+      randomMealIngredients.strMeasure5,
+      randomMealIngredients.strMeasure6,
+      randomMealIngredients.strMeasure7,
+      randomMealIngredients.strMeasure8,
+      randomMealIngredients.strMeasure9,
+      randomMealIngredients.strMeasure10,
+      randomMealIngredients.strMeasure11,
+      randomMealIngredients.strMeasure12,
+      randomMealIngredients.strMeasure13,
+      randomMealIngredients.strMeasure14,
+      randomMealIngredients.strMeasure15,
+      randomMealIngredients.strMeasure16,
+      randomMealIngredients.strMeasure17,
+      randomMealIngredients.strMeasure18,
+      randomMealIngredients.strMeasure19,
+      randomMealIngredients.strMeasure20,
+    ];
+    let randomMealIngredientsArr = [
+      randomMealIngredients.strIngredient1,
+      randomMealIngredients.strIngredient2,
+      randomMealIngredients.strIngredient3,
+      randomMealIngredients.strIngredient4,
+      randomMealIngredients.strIngredient5,
+      randomMealIngredients.strIngredient6,
+      randomMealIngredients.strIngredient7,
+      randomMealIngredients.strIngredient8,
+      randomMealIngredients.strIngredient9,
+      randomMealIngredients.strIngredient10,
+      randomMealIngredients.strIngredient11,
+      randomMealIngredients.strIngredient12,
+      randomMealIngredients.strIngredient13,
+      randomMealIngredients.strIngredient14,
+      randomMealIngredients.strIngredient15,
+      randomMealIngredients.strIngredient16,
+      randomMealIngredients.strIngredient17,
+      randomMealIngredients.strIngredient18,
+      randomMealIngredients.strIngredient19,
+      randomMealIngredients.strIngredient20,
+    ];
+    randomMealMeasurementArr.forEach((measurement) => {
+      if (measurement !== "") {
+        $("#randomMealMeasurement").append(`<li>${measurement}</li>`);
+      }
+    });
+
+    randomMealIngredientsArr.forEach((ingredient) => {
+      if (ingredient !== "") {
+        $("#randomMealIngredient").append(`<li>${ingredient}</li>`);
+      }
+    });
 
     let mealIngredient =
       response.meals[0].strMeasure1 +
@@ -49,7 +112,7 @@ $("#random-meal").on("click", function () {
         " " +
         response.meals[0].strIngredient4 +
         " ,") +
-      (+ " " +
+      (+" " +
         response.meals[0].strMeasure5 +
         " " +
         response.meals[0].strIngredient5 +
@@ -74,14 +137,16 @@ $("#random-meal").on("click", function () {
         " " +
         response.meals[0].strIngredient9 +
         " ,") +
-      (" " +response.meals[0].strMeasure10 +
+      (" " +
+        response.meals[0].strMeasure10 +
         " " +
         response.meals[0].strIngredient10 +
         " ,") +
       (" " +
         response.meals[0].strMeasure11 +
         " " +
-        response.meals[0].strIngredient11 + " ") +
+        response.meals[0].strIngredient11 +
+        " ") +
       (" ," +
         response.meals[0].strMeasure12 +
         " " +
@@ -110,8 +175,8 @@ $("#random-meal").on("click", function () {
       (" " +
         response.meals[0].strMeasure17 +
         " " +
-        response.meals[0].strIngredient17) + 
-        (" " +
+        response.meals[0].strIngredient17) +
+      (" " +
         response.meals[0].strMeasure18 +
         " " +
         response.meals[0].strIngredient18 +
@@ -125,11 +190,11 @@ $("#random-meal").on("click", function () {
         response.meals[0].strMeasure20 +
         " " +
         response.meals[0].strIngredient20 +
-        " ,")
+        " ,");
 
-    console.log(mealIngredient);
+    // console.log(mealIngredient);
     let ingredients = $("<secttion/>");
-    ingredients.text('The ingredeients are :' + mealIngredient);
+    ingredients.text("The ingredeients are :" + mealIngredient);
     ingredients.appendTo(".h4");
 
     //   // recipe steps
@@ -149,31 +214,37 @@ $("#random-meal").on("click", function () {
 
     // });
     let cookingInstructions = response.meals[0].strInstructions;
-    console.log(cookingInstructions);
+    // console.log(cookingInstructions);
     let instructions = $("<section>");
     instructions.text(cookingInstructions);
     instructions.appendTo(".p-3");
-    console.log(cookingInstructions);
+    // console.log(cookingInstructions);
 
     // meal image
     let mealImage = response.meals[0].strMealThumb; // calling the image object
-    var randomMealimage = $("<img/>"); // creating an image tag for the image object
+    // var randomMealimage = $("<img/>"); // creating an image tag for the image object
     //setting attributes for the image tags
-    randomMealimage.attr("src", mealImage);
-    randomMealimage.attr("width", 300);
-    randomMealimage.attr("height", 300);
-    console.log(randomMealimage);
-    randomMealimage.appendTo("#images"); // adding the image to the image div in the html
+    // randomMealimage.attr("src", mealImage);
+    randomMealImage.attr("src", mealImage);
+    randomMealImage.attr("width", 300);
+    randomMealImage.attr("height", 300);
+    // console.log(randomMealImage);
+    // randomMealimage.appendTo("#images"); // adding the image to the image div in the html
 
     // creating video link for the meal
-    console.log(response.meals[0].strYoutube);
+    // console.log(response.meals[0].strYoutube);
     let videoId = response.meals[0].strYoutube.split("="); //using the split function to get url and the vid id number
-    console.log(videoId);
+    // console.log(videoId);
     let vidSource = "https://www.youtube.com/embed/" + videoId[1]; // used the embeded video link url and added the videoID number to call the video
-    console.log(vidSource);
-    let iFrame = document.createElement("iframe"); // an iframe element is created to display the video on to the webpage
-    iFrame.src = vidSource; // the iframe src will be the vidSource link
+    // console.log(vidSource);
+    let iFrame = $("<iframe>"); // an iframe element is created to display the video on to the webpage
+    // iFrame.src = vidSource; // the iframe src will be the vidSource link
     // document.getElementById(".video").appendChild(iFrame)
-    document.body.div.video.appendChild(iFrame); // appending(adding) the video source on to the html document
+    iFrame.attr("src", vidSource);
+    iFrame.attr("width", 300);
+    iFrame.attr("height", 300);
+    iFrame.empty().appendTo("#ramdom-meal-video");
+
+    // document.body.div.video.appendChild(iFrame); // appending(adding) the video source on to the html document
   });
 });
